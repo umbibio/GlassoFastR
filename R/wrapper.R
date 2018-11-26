3a296576b5bcdbde1fed9f27670b048029bbc841
Amir Vajdi <amirvajdi91@gmail.com>
2018-11-26 16:07:45 -0500

    Update wrapper.R

--- R/wrapper.R
+++ R/wrapper.R
@@ -1,6 +1,6 @@
 
 glassoParallel=function(S,RhoS,approx=-1,shrink=-1,threads,cutoffs=0.0001){
-dyn.load("src/psicov.so")
+dyn.load("src/GlassoFastParallel.so")
 
 ptm <- proc.time()
 out=.C("main",cov=as.numeric(S),L=as.numeric(RhoS),size=as.integer(nrow(S)),approximation=as.integer(approx),
